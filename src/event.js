856751c3095abaa70b25a4b3c523b483d6d889a7
Manpreet Singh <92616694+mskhurana728@users.noreply.github.com>
2025-01-15 01:38:53 -0500

    feat: create a event to listent o addListButton
    This function will validate the listName input and also add the new list to lists array and then in last will render it on the screen

--- src/event.js
+++ src/event.js
@@ -1,13 +1,36 @@
+import { Validate } from "./valid.js";
+import { List } from "./lists.js";
+import { Render } from "./render.js";
 export const events = (() => {
   const newListButton = document.querySelector(".newListButton");
-  const newListInputContainer = document.querySelector(
-    ".newListInputContainer"
-  );
+
+  const addListButton = document.querySelector(".addListButton");
+  //pseudocode
+  //We want to create an event on whenever add list button is clicked
+  //it will first validate weather the input is not empty, if it is empty then it will show the error
+  //then when the user put the name correctly and clicks add list button it will erase the error
+  //and also add the list to the lists array;
+  //change the display property of newListButton and newListInputContainer
+  // and will display the list name in the listRendering area
+
+  const addListButtonEvent = (function () {
+    addListButton.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      let newListName = Validate.validateNewListNameInput();
+      if (!newListName) {
+        return;
+      }
+
+      List.addListToListsArray(newListName);
+      Render.toggleNewListInputContainerAndButtonDisplay();
+      Render.displayLists();
+    });
+  })();
 
   const newListButtonEvent = (function () {
     newListButton.addEventListener("click", (e) => {
-      newListInputContainer.style.display = "block";
-      newListButton.style.display = "none";
+      Render.toggleNewListInputContainerAndButtonDisplay();
     });
   })();
 })();
