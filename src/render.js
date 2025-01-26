98aacf90ef4f72ecbecdba4f9c851149a67945fc
Manpreet Singh <92616694+mskhurana728@users.noreply.github.com>
2025-01-26 12:02:09 -0500

    feat: create removeError to remove error from input
    This function will remove errors from the provided area if they have any

--- src/render.js
+++ src/render.js
@@ -60,4 +60,70 @@ export class Render {
     events.removeListButtonEvent();
     events.selectListEvent();
   }
+  static showError(area) {
+    const errorSpan = area.childNodes[4];
+    if (errorSpan) {
+      errorSpan.textContent = "Required";
+      return;
+    }
+    let newErrorSpan = document.createElement("span");
+    newErrorSpan.textContent = "Required";
+    newErrorSpan.classList.add(".errorSpan");
+    area.appendChild(newErrorSpan);
+  }
+
+  static removeError(area) {
+    const errorSpan = area.childNodes[4];
+    if (errorSpan) {
+      errorSpan.textContent = "";
+    }
+  }
+  //we want to display all the tasks in the selected list
+  //a task should have a check mark to change the status to complete and Tittle , due date and a delete button
+  //whenever the task is clicked it should expand and show task notes which should be editable
+
+  static createTaskElements(task, counter) {
+    const taskDiv = document.createElement("div");
+    taskDiv.classList.add("taskContainer");
+    const taskCheckbox = document.createElement("input");
+    taskCheckbox.classList.add("taskCheckbox");
+    taskCheckbox.setAttribute("data-taskIndex", counter);
+
+    taskDiv.appendChild(taskCheckbox);
+    const taskTittle = document.createElement("span");
+    taskTittle.classList.add("taskTittle");
+    taskTittle.textContent = task.tittle;
+    taskDiv.appendChild(taskTittle);
+    const taskRemoveButton = document.createElement("button");
+    taskRemoveButton.classList.add("taskRemoveButton");
+    taskRemoveButton.setAttribute("data-taskIndex", counter);
+    taskRemoveButton.textContent = "X";
+    taskDiv.appendChild(taskRemoveButton);
+
+    const taskDueDate = document.createElement("span");
+    taskDueDate.classList.add(".taskDueDate");
+    taskDueDate.textContent = task.dueDate;
+    taskDiv.appendChild(taskDueDate);
+
+    const taskNotes = document.createElement("p");
+    taskNotes.classList.add("taskNotes");
+    taskNotes.textContent = task.notes;
+    taskDiv.appendChild(taskNotes);
+
+    return taskDiv;
+  }
+
+  static displaySelectedListTasks() {
+    let selectedList = List.selectedList;
+    const taskRenderingArea = document.querySelector(".tasksRenderingArea");
+    taskRenderingArea.textContent = "";
+    // taskRenderingArea.textContent = List.lists[selectedList][0];
+    // console.log(List.lists[selectedList]);
+    let counter = 0;
+    List.lists[selectedList].forEach((task) => {
+      let taskToBeDisplayed = this.createTaskElements(task, counter);
+      counter++;
+      taskRenderingArea.appendChild(taskToBeDisplayed);
+    });
+  }
 }
--- src/valid.js
+++ src/valid.js
@@ -11,4 +11,42 @@ export class Validate {
 
     return newListName;
   }
+
+  static validateTaskInputs() {
+    const tittle = document.querySelector("#taskTittle").value;
+    const description = document.querySelector("#taskDescription").value;
+    const dueDate = document.querySelector("#taskDueDate").value;
+    let isValid = true;
+    let taskTittleInputContainer = document.querySelector(
+      ".taskTittleInputContainer"
+    );
+
+    const taskDescriptionInputContainer = document.querySelector(
+      ".taskDescriptionInputContainer"
+    );
+
+    const taskDueDateInputContainer = document.querySelector(
+      ".taskDueDateInputContainer"
+    );
+
+    if (!tittle) {
+      Render.showError(taskTittleInputContainer);
+      isValid = false;
+    } else {
+      Render.removeError(taskTittleInputContainer);
+    }
+    if (!description) {
+      Render.showError(taskDescriptionInputContainer);
+      isValid = false;
+    } else {
+      Render.removeError(taskDescriptionInputContainer);
+    }
+    if (!dueDate) {
+      Render.showError(taskDueDateInputContainer);
+      isValid = false;
+    } else {
+      Render.removeError(taskDueDateInputContainer);
+    }
+    return isValid;
+  }
 }
