20331690f457c459d99db0443472ca58514139b6
Manpreet Singh <mskhurana729@gmail.com>
2025-01-29 02:19:27 -0500

    feat: Create a func to save lists to local storage

---
+++ src/localstorage.js
@@ -0,0 +1,9 @@
+//so what we want is whenever a new list or task is created it should be saved in the local storage
+//to do so we have to update addList event and addTask event
+// we will have to create a function which will save the list to local storage whenever a new task or list is created
+
+export class LocalStorage {
+  static saveListToLocalStorage(list) {
+    localStorage.setItem("List", JSON.stringify(list));
+  }
+}
